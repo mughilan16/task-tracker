215c70ff0d26e87542dffd7386aceb33b5d3d66d
mughilan16 <mughilan16052005@gmail.com>
2024-06-16 15:26:17 +0530

    update flags and update README

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# task-tracker
 ## Usage of tt:
-  ### -complete
+  ### complete
         complete the current task
-  ### -start string
+  ### export
+        export task to csv
+  ### month int
+        month for query (default 6)
+  ### start string
         start a new task
+  ### tag string
+        tag for work, personal. default tag: work. all tag only should be used when querying (default "work")
+  ### total
+        show total for month
+  ### year int
+        year for query (default 2024)
--- app/flags.go
+++ app/flags.go
@@ -7,37 +7,44 @@ import (
 )
 
 func (app App) InitFlags() {
-	var start string
-	var complete bool
-	var total bool
-  var export bool
+	var start, complete, total, export bool
+	var message, tag string
 	var month, year int
-  var tag string
-	flag.StringVar(&start, "start", "", "start a new task")
+	flag.BoolVar(&start, "start", false, "start a new task")
+	flag.StringVar(&message, "m", "", "description of task")
 	flag.IntVar(&month, "month", int(time.Now().Month()), "month for query")
 	flag.IntVar(&year, "year", int(time.Now().Year()), "year for query")
 	flag.BoolVar(&complete, "complete", false, "complete the current task")
-  flag.StringVar(&tag, "tag", "work", "tag for work, personal. default tag: work. all tag only should be used when querying")
+	flag.StringVar(&tag, "tag", "work", "tag for work, personal. default tag: work. all tag only should be used when querying")
 	flag.BoolVar(&total, "total", false, "show total for month")
-  flag.BoolVar(&export, "export", false, "export task to csv")
+	flag.BoolVar(&export, "export", false, "export task to csv")
 	flag.Parse()
-  if tag != "work" && tag != "personal" && tag != "all" {
-    fmt.Println("Invalid tag")
-    return
-  }
-	if start != "" {
-    if tag == "all" {
-      fmt.Println("all tag should not be used when creating new task")
-    }
-		app.db.AddNewTask(start, tag)
+	if tag != "work" && tag != "personal" && tag != "all" {
+		fmt.Println("Invalid tag")
+		return
+	}
+	if start {
+		if tag == "all" {
+			fmt.Println("all tag should not be used when creating new task")
+			return
+		}
+		if message == "" {
+			fmt.Println("description should be provided to create a new task")
+			return
+		}
+		app.db.AddNewTask(message, tag)
+		return
 	}
 	if complete {
 		app.db.CompleteTask()
+		return
 	}
 	if total {
 		app.db.TotalForMonth(month, year, tag)
+		return
 	}
-  if export {
-    app.db.Export(month, year, tag)
-  }
+	if export {
+		app.db.Export(month, year, tag)
+		return
+	}
 }
